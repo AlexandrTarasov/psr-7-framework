2c29b62970ca0846149fe4651a2844e878dd4111
ATarasov <usproinfo@gmail.com>
2019-10-27 00:19:34 +0300

    first commit

---
+++ tests/Framework/Http/RequestTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Framework\Http;
+
+use Framework\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+	public function setUp() :void
+	{
+		parent::setUp();
+		$_GET=[];
+		$_POST = [];
+	}
+	public function testEmpty() : void
+	{
+		$request = new Request();
+		self::assertEquals([], $request->getQueryParams());
+		self::assertNull($request->getParsedBody());
+	}
+
+	public function testQueryparams() : void
+	{
+		$_GET = $data = [
+			'name' => 'John',
+			'age' => 28,
+		];
+
+		$request = new Request();
+
+		self::assertEquals($data, $request->getQueryParams());
+		self::assertNull($request->getParsedBody());
+
+	}
+
+	public function testParsedBody(): void
+	{
+		$_POST = $data = ['title' => 'Title'];
+		$request = new Request();
+
+		self::assertEquals([], $request->getQueryParams());
+		self::assertEquals($data, $request->getParsedBody());
+	}
+}
