308bdbd4f2b88938c6659171c25a5c777efd58ee
ATarasov <usproinfo@gmail.com>
2019-10-29 22:57:54 +0200

    delete native request and response

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
-use Zend\Diactoros\Response\HtmlResonse;
+use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\ServerRequestFactory;
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -9,7 +13,7 @@ require 'vendor/autoload.php';
 $request = ServerRequestFactory::fromGlobals();
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-$response = (new HtmlResonse('Hello,' . $name . '!'))
+$response = (new HtmlResponse('Hello,' . $name . '!'))
 	->withHeader('X-developer', 'Alex T');
 
 header('HTTP/1.0 '. $response->getStatusCode() . ' ' . $response->getReasonPhrase());
