51400e405bc713dc3801129e49f126502cbce178
ATarasov <usproinfo@gmail.com>
2019-12-04 23:12:28 +0200

    simple authentification system

--- public/index.php
+++ public/index.php
@@ -18,12 +18,19 @@ use Framework\Http\Router\AuraRouterAdapter;
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
+$params = [
+    'users' => ['admin' => 'password'],
+];
+
+
 $aura = new Aura\Router\RouterContainer();
 $routes = $aura->getMap();
+
 $routes->get('home', '/', Action\HelloAction::class);
 $routes->get('about', '/about', Action\AboutAction::class);
 $routes->get('blog', '/blog', Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
+$routes->get('cabinet', '/cabinet', new Action\CabinetAction($params['users']));
 
 $router = new AuraRouterAdapter($aura);
 $resolver = new ActionResolver();
